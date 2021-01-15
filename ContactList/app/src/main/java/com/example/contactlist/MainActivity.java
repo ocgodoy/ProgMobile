728ec1b4ab35c296375c4d3f3ea98a0b9fdfdf49
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2021-01-15 16:53:31 +0100

    app1 presque finie erreur d'import de variable du fragmentcontainerview

--- ContactList/app/src/main/java/com/example/contactlist/MainActivity.java
+++ ContactList/app/src/main/java/com/example/contactlist/MainActivity.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.provider.ContactsContract;
+
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("some_int", 0);
+
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, ContactsFragment.class, null)
+                    .commit();
+        }
     }
 }
